77b33930a200724e0fecb7f681b1b19419309c30
Christopher Orth <christopherorth430@yahoo.com>
2023-08-23 16:51:33 -0400

    did the functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+saturdayFun('bathe my dog');
+
+function saturdayFun(string) {
+    if (string) {
+        return `This Saturday, I want to ${string}!`
+    } else {
+        return `This Saturday, I want to roller-skate!`;
+    }
+};
+
+function mondayWork(string) {
+    if (string) {
+        return `This Monday, I will ${string}.`
+    } else {
+            return `This Monday, I will go to the office.`;
+        }
+};
+
+function wrapAdjective(symbol = "special") {
+    return function (word) {
+        return `You are ${symbol}${word}${symbol}!`;
+    }
+};
